1cb963899edddd8e06e41549bfb3ec58267a9bed
Cherhoune Badr <badr.cherhoune@capgemini.com>
2023-07-13 15:34:30 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './componants/user/login/login.component';
+import { RegisterComponent } from './componants/user/register/register.component';
+import { ListComponent } from './componants/outils/list/list.component';
+import { NotfoundComponent } from './componants/outils/notfound/notfound.component';
+import { NavComponent } from './componants/nav/nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "" , component: LoginComponent},
+  {path: "register" , component: RegisterComponent},
+  {path: "list" , component: ListComponent},
+  { path: '**', pathMatch: 'full', component: NotfoundComponent },
+  {path: "nav" , component: NavComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './componants/nav/nav.component';
+import { LoginComponent } from './componants/user/login/login.component';
+import { RegisterComponent } from './componants/user/register/register.component';
+import { NotfoundComponent } from './componants/outils/notfound/notfound.component';
+import { FormComponent } from './componants/outils/form/form.component';
+import { ListComponent } from './componants/outils/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotfoundComponent,
+    FormComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
